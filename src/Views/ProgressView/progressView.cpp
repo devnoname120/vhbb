27758ba10a9421271584eea963ed6bb1d1516b5f
Arkanite <arkaniteonvita@gmail.com>
2017-03-30 16:03:48 +1000

    applied new dialog layout to ProgressView::Display()

--- src/Views/ProgressView/progressView.cpp
+++ src/Views/ProgressView/progressView.cpp
@@ -3,12 +3,20 @@
 #include <font.h>
 
 extern unsigned char _binary_assets_spr_img_dialog_progress_bg_png_start;
+extern unsigned char _binary_assets_spr_img_dialog_progress_bar_png_start;
+extern unsigned char _binary_assets_spr_img_dialog_progress_bar_glow_png_start;
+extern unsigned char _binary_assets_spr_img_dialog_btn_png_start;
 
 
-ProgressView::ProgressView(InfoProgress progress) :
+ProgressView::ProgressView(InfoProgress progress, Homebrew hb) :
     progress_(progress),
-	font_25(Font(std::string(FONT_DIR "segoeui.ttf"), 25)),
-    img_dialog_progress_bg(Texture(&_binary_assets_spr_img_dialog_progress_bg_png_start))
+	font_24(Font(std::string(FONT_DIR "segoeui.ttf"), 24)),
+	hb_(hb),
+	img_icon(Texture(ICONS_FOLDER + "/" + hb_.icon)),
+    img_dialog_progress_bg(Texture(&_binary_assets_spr_img_dialog_progress_bg_png_start)),
+	img_dialog_progress_bar(Texture(&_binary_assets_spr_img_dialog_progress_bar_png_start)),
+	img_dialog_progress_bar_glow(Texture(&_binary_assets_spr_img_dialog_progress_bar_glow_png_start)),
+	img_dialog_btn(Texture(&_binary_assets_spr_img_dialog_btn_png_start))
 {
 }
 
@@ -19,12 +27,23 @@ int ProgressView::HandleInput(int focus, const Input& input)
 
 int ProgressView::Display()
 {
+	float progress_x = ((float)PROGRESS_BAR_X_WIDTH) * ((float)progress_.percent() / (float)100);
+	// Icon
+	img_icon.Draw(Point(PROGRESS_VIEW_X + 45, PROGRESS_VIEW_Y + 7));
+	// Background
 	img_dialog_progress_bg.Draw(Point(PROGRESS_VIEW_X, PROGRESS_VIEW_Y));
-    float progress_x = ((float)PROGRESS_BAR_X_WIDTH) * ((float)progress_.percent() / (float)100);
-    vita2d_draw_rectangle(PROGRESS_VIEW_X + PROGRESS_BAR_X, PROGRESS_VIEW_Y + PROGRESS_BAR_Y, progress_x, PROGRESS_BAR_Y_HEIGHT, COLOR_BLUEGREY);
-    
-    font_25.Draw(Point(PROGRESS_VIEW_X + PROGRESS_BAR_X, PROGRESS_VIEW_Y + 72), progress_.message());
-
+	// Name
+	font_24.Draw(Point(PROGRESS_VIEW_X + 197, PROGRESS_VIEW_Y + 53), hb_.name);
+	// Message
+	font_24.Draw(Point(PROGRESS_VIEW_X + 197, PROGRESS_VIEW_Y + 117), progress_.message());
+    // Progress bar
+    img_dialog_progress_bar.DrawResize(Point(PROGRESS_VIEW_X + 55, PROGRESS_VIEW_Y + 148), Point(progress_x, 11));
+	img_dialog_progress_bar_glow.Draw(Point(PROGRESS_VIEW_X + progress_x + 5, PROGRESS_VIEW_Y + 104));
+	// Cancel button
+	img_dialog_btn.Draw(Point(PROGRESS_VIEW_X + 148, PROGRESS_VIEW_Y + 178));
+	// Percent (if required)
+	//font_24.Draw(Point(PROGRESS_VIEW_X + 197, PROGRESS_VIEW_Y + 117), progress_.percent());
+	
     if (finish_tick != 0 && sceKernelGetProcessTimeLow() > finish_tick) request_destroy = true;
 	return 0;
 }
