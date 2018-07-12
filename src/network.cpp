7860b45980a9ec4d0af8062e42aa09e0effcf0af
devingDev <bizzy933@gmail.com>
2018-07-12 11:26:59 +0200

    Readded download getting download size. Made progress function work by passing the pointer to InfoProgress in the curl request.

--- src/network.cpp
+++ src/network.cpp
@@ -84,7 +84,9 @@ static size_t write_data_to_disk(void *ptr, size_t size, size_t nmemb, void *str
 }
 
 static int progress_callback(void *clientp,   double dltotal,   double dlnow,   double ultotal,   double ulnow){
-	return 0;
+	InfoProgress * progress = (InfoProgress *)clientp;
+	if(progress) progress->percent((float)(dlnow/dltotal) * 100);
+	return CURLE_OK;
 }
 
 
@@ -95,11 +97,79 @@ int Network::DownloadSize(std::string url, uint64_t *size, InfoProgress progress
 
 int Network::DownloadSize(std::string url, uint64_t *size, InfoProgress *progress)
 {
+	CURL *curl;
+	CURLcode res;
+	curl = curl_easy_init();
+	if(curl) {
+		struct stringcurl body;
+		init_string(&body);
+		struct stringcurl header;
+		init_string(&header);
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		// Set useragant string
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+		// not sure how to use this when enabled
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+		// not sure how to use this when enabled
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+		// Set SSL VERSION to TLS 1.2
+		curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+		// Set timeout for the connection to build
+		curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 10L);
+		// Follow redirects (?)
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		
+		curl_easy_setopt(curl, CURLOPT_NOBODY, 1);
+		
+		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+		// The object to pass to the progress callback 
+		curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, progress);
+		// The function that will be used to write the data 
+		curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progress_callback);
+		// write function of response headers
+		curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, writefunc);
+		// the response header data where to write to
+		curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header);
+		// Request Header :
+		struct curl_slist *headerchunk = NULL;
+		headerchunk = curl_slist_append(headerchunk, "Accept: */*");
+		headerchunk = curl_slist_append(headerchunk, "Content-Type: application/json");
+		headerchunk = curl_slist_append(headerchunk, "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+		//headerchunk = curl_slist_append(headerchunk, "Host: discordapp.com");  Setting this will lead to errors when trying to download. should be set depending on location : possible : cdn.discordapp.com or images.discordapp.com
+		headerchunk = curl_slist_append(headerchunk, "Content-Length: 0");
+		res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerchunk);
+		
+		
+		// Perform the request
+		res = curl_easy_perform(curl);
+		int httpresponsecode = 0;
+		curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &httpresponsecode);
+		
+		
+		//  This is the value read from the Content-Length: field. Stores -1 if the size isn't known.
+		curl_off_t clDownloadSize;
+		curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD , &clDownloadSize);
+		
+		if(res != CURLE_OK){
+			//fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+			
+		}else{
+			//printf("Download size: %" CURL_FORMAT_CURL_OFF_T "\n", clDownloadSize);
+		}
+		
+		
+	}else{
+		
+	}
+
+	// cleanup
+	curl_easy_cleanup(curl);
     return 0;
 }
 
 int Network::Download(std::string url, std::string dest, InfoProgress *progress)
 {
+	if(progress) progress->message("Starting the download... ");
 	int fileFD = sceIoOpen( dest.c_str(), SCE_O_WRONLY | SCE_O_CREAT, 0777);
 	if(!fileFD){
 		return 0;
@@ -127,9 +197,11 @@ int Network::Download(std::string url, std::string dest, InfoProgress *progress)
 		// Follow redirects (?)
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
 		// The function that will be used to write the data 
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_to_disk);
+		// The object to pass to the progress callback 
+		curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, progress);
 		// The progress callback
 		curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progress_callback);
 		// The data filedescriptor which will be written to
@@ -147,6 +219,7 @@ int Network::Download(std::string url, std::string dest, InfoProgress *progress)
 		headerchunk = curl_slist_append(headerchunk, "Content-Length: 0");
 		res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headerchunk);
 		
+		if(progress) progress->message("Downloading...");
 		
 		// Perform the request
 		res = curl_easy_perform(curl);
