232083c1c535759cdec3a501db4ad84817d80a8b
devnoname120 <devnoname120+gh@gmail.com>
2018-08-02 17:22:11 +0200

    remove useless logging

--- src/shapes.cpp
+++ src/shapes.cpp
@@ -10,8 +10,7 @@ Point::Point(double _x, double _y)
 int Rectangle::Inside(const Point &pt) const
 {
 	dbg_printf(DBG_DEBUG, "Inside point: %f,%f", pt.x, pt.y);
-	dbg_printf(DBG_DEBUG, "Cond is %d", (int)(pt.x >= topLeft.x && pt.x <= bottomRight.x
-		&& pt.y >=topLeft.y && pt.y <= bottomRight.y));
+
 	return pt.x >= topLeft.x && pt.x <= bottomRight.x
 		&& pt.y >=topLeft.y && pt.y <= bottomRight.y;
 }
