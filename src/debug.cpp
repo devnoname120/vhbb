4e39829edd9e6ff172db1d8e22a50c799350cc5c
devnoname120 <devnoname120+gh@gmail.com>
2019-06-17 23:41:05 +0200

    Fix logging warning

--- src/debug.cpp
+++ src/debug.cpp
@@ -73,22 +73,22 @@ int _log_printf(int level, const char *format, ...)
 	char buf[512];
 	vsnprintf(buf, 512, format, args);
 
-	char buf_colored[512];
+	char buf_colored[600];
 	switch (level) {
 	case DBG_INFO:
-		snprintf(buf_colored, 512, "\033[1;34;7m[INFO]\033[0m\033[1;34m %s\033[0m", buf);
+		snprintf(buf_colored, 600, "\033[1;34;7m[INFO]\033[0m\033[1;34m %s\033[0m", buf);
 		break;
 	case DBG_ERROR:
-		snprintf(buf_colored, 512, "\033[1;31;7m[ERROR]\033[0m\033[1;31m %s\033[0m", buf);
+		snprintf(buf_colored, 600, "\033[1;31;7m[ERROR]\033[0m\033[1;31m %s\033[0m", buf);
 		break;
 	case DBG_WARNING:
-		snprintf(buf_colored, 512, "\033[1;35;7m[WARNING]\033[0m\033[1;35m %s\033[0m", buf);
+		snprintf(buf_colored, 600, "\033[1;35;7m[WARNING]\033[0m\033[1;35m %s\033[0m", buf);
 		break;
 	case DBG_DEBUG:
-		snprintf(buf_colored, 512, "\033[1;30;7m[DEBUG]\033[0m\033[1;30m %s\033[0m", buf);
+		snprintf(buf_colored, 600, "\033[1;30;7m[DEBUG]\033[0m\033[1;30m %s\033[0m", buf);
 		break;
 	default:
-        snprintf(buf_colored, 512, "\033[1;31;7m[UNK]\033[0m\033[1;31m %s\033[0m", buf);
+        snprintf(buf_colored, 600, "\033[1;31;7m[UNK]\033[0m\033[1;31m %s\033[0m", buf);
 	    break;
     }
 
--- src_updater/debug.cpp
+++ src_updater/debug.cpp
@@ -7,7 +7,6 @@
 
 #include <psp2/io/stat.h>
 #include <psp2/kernel/modulemgr.h>
-#include <psp2/kernel/processmgr.h>
 #include <psp2/io/fcntl.h>
 #include <psp2/rtc.h>
 
@@ -81,22 +80,22 @@ int _log_printf(int level, const char *format, ...) {
 	va_end(args);
 
 #ifdef DEBUGNET
-	char buf_colored[512];
+	char buf_colored[600];
 	switch (level) {
 		case DBG_INFO:
-			snprintf(buf_colored, 512, "\033[1;34;7m[INFO]\033[0m\033[1;34m %s\033[0m", buf);
+			snprintf(buf_colored, 600, "\033[1;34;7m[INFO]\033[0m\033[1;34m %s\033[0m", buf);
 			break;
 		case DBG_ERROR:
-			snprintf(buf_colored, 512, "\033[1;31;7m[ERROR]\033[0m\033[1;31m %s\033[0m", buf);
+			snprintf(buf_colored, 600, "\033[1;31;7m[ERROR]\033[0m\033[1;31m %s\033[0m", buf);
 			break;
 		case DBG_WARNING:
-			snprintf(buf_colored, 512, "\033[1;35;7m[WARNING]\033[0m\033[1;35m %s\033[0m", buf);
+			snprintf(buf_colored, 600, "\033[1;35;7m[WARNING]\033[0m\033[1;35m %s\033[0m", buf);
 			break;
 		case DBG_DEBUG:
-			snprintf(buf_colored, 512, "\033[1;30;7m[DEBUG]\033[0m\033[1;30m %s\033[0m", buf);
+			snprintf(buf_colored, 600, "\033[1;30;7m[DEBUG]\033[0m\033[1;30m %s\033[0m", buf);
 			break;
 		default:
-			snprintf(buf_colored, 512, "\033[1;31;7m[UNK]\033[0m\033[1;31m %s\033[0m", buf);
+			snprintf(buf_colored, 600, "\033[1;31;7m[UNK]\033[0m\033[1;31m %s\033[0m", buf);
 			break;
 	}
 	debugNetUDPSend(buf_colored);
